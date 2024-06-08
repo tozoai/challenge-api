8d7e4a53272edda6791a6fc39319da54a9f1373b
IAnuncios <tozoartintel@gmail.com>
2024-06-08 15:14:46 -0300

    brings full collection from the mongo with get

--- main.py
+++ main.py
@@ -1,44 +1,60 @@
-import flask
-from flask import request, jsonify
+from flask import Flask, request, json, Response
+from pymongo import MongoClient
+from pydantic import BaseModel, Field, EmailStr
+from typing import Optional
+from bson import ObjectId
+from bson.json_util import dumps
+from flask import jsonify
 
-app = flask.Flask(__name__)
+
+app = Flask(__name__)
 app.config["DEBUG"] = True
 
-provedores = [
-    {
-        "id": 1,
-        "Nombre":"Fornecedor 1",
-        "Razon_Social":"Razon Social 1",
-        "Nombre_Contato":"Nombre del contato 1",
-        "email":"email@contato1.com",
-        "Direccion_Fiscal":"Direccion contato 1",
-        "Tipo_Servicio":"Tipo Servicio Contato 1",
-        "Criticidad":"Baja",
-        "role":"usuario"
-    },
-    {
-        "id": 2,
-        "Nombre":"Nombre 2",
-        "Razon_Social":"Razon Social 2",
-        "Nombre_Contato":"Nombre del contato 2",
-        "email":"email@contato2.com",
-        "Direccion_Fiscal":"Direccion contato 2",
-        "Tipo_Servicio":"Tipo Servicio Contato 2",
-        "Criticidad":"Alta",
-        "role":"administrador"
-    }
-]
+class PydanticObjectId(ObjectId):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):
+        if not ObjectId.is_valid(v):
+            raise ValueError("Invalid objectid")
+        return ObjectId(v)
+
+    @classmethod
+    def __modify_schema__(cls, field_schema):
+        field_schema.update(type="string")
+
+class Proveedores(BaseModel):
+    id: int
+    Nombre: str
+    Razon_Social: str
+    Nombre_Contato: str
+    email: EmailStr
+    Direccion_Fiscal: str
+    Tipo_Servicio: str
+    Criticidad: str
+    
+client = MongoClient("mongodb://localhost:5000/data?retryWrites=true&w=majority")
+
+db = client['data'] 
+collection = db['proveedores']
+
+@app.route('/proveedores', methods=['GET'])
+
+def get_proveedores():
+
+    proveedores = list(collection.find())
+    return jsonify(dumps(proveedores))  # convert BSON to JSON
+
 
 @app.route('/', methods=['GET'])
 
 def home():
     return '''<head>API Challenge</head><br>
-      <p>This is the default API Response   </p>'''
-
-@app.route('/api/v1/provedores', methods=['GET'])
-
-def mostra_provedores():
-    return jsonify(provedores)
+      <p>This is the default API Response   </p>
+      
+      '''
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=20000)
