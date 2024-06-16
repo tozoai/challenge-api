99719edcbea75ef8528cdd3f90635dfc80b77a8a
IAnuncios <tozoartintel@gmail.com>
2024-06-16 13:55:28 -0300

    Trying to auto deploy on ECS

--- build.bat
+++ build.bat
@@ -1,3 +1,8 @@
+copilot app init
+copilot env init --name newapp --default-config --profile default
+copilot svc init --app newapp --name db mongo:latest --svc-type "Backend Service" --port 27017
+copilot svc init --app newapp --name web --dockerfile api/Dockerfile --svc-type "Load Balanced Web Service" --port 8080
+
 copilot env deploy --name newapp
 copilot svc deploy --name db --env newapp
 copilot svc deploy --name web --env newapp
